2b77e4634c507f1330b0b968a7a8cd55efdb6367
julius <leonardojulius.jcl@gmail.com>
2020-05-19 15:20:37 +0800

    Layouting

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Testing</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content ="width=device-width, initial-sacle=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Front Dev Course</title>
+    <link rel="stylesheet"  href="css/main.css">
 </head>
 
-<?php
+<body>
 
- echo phpinfo();
-?>
+<div class="container">
+    <header>
+        <img src="images/logo.svg" alt="23andfour logo">
+
+        <nav>
+            <a href="" class="hide-desktop">
+                <img src="images/ham.svg" alt="toggle menu" class="menu" id="menu">
+            </a>
+
+            <ul class="show-desktop hide-desktop" id="nav">
+            
+                <li id="exit" class="exit-btn hide-desktop"><img src="images/exit.svg" alt="exit menu"></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                
+            </ul>
+
+        </nav>
+    </header> 
+
+</div>
+
+</body>
 </html>
